3273ae7bf18b628cc397f2c7360914cbefd5d243
Kailash_mms <srikarkailash9@gmail.com>
2023-12-21 19:12:54 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div>
+        <img src="images/image-qr-code.png" alt="qr" />
+      </div>
+      <div>
+        <h3 className='desc'>Improve your front-end skills by building projects</h3>
+        <p className='desc2'>Scan the QR code to visit Frontend Mentor and take your coding skills to the next level</p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
