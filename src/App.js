719352bc9fa53c445a8f6d6f389c037e503e8571
Kailash_mms <srikarkailash9@gmail.com>
2023-12-22 19:27:59 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div>
+        <img src="assets/images/illustration-article.svg" alt="qr" />
+      </div>
+      
+      <h3 className="badge">Learning</h3>
+      
+      <p>Published 21 Dec 2023</p>
+
+      <h2 className="heading">HTML & CSS foundations</h2>
+        These languages are the backbone of every website, defining structure, content, and presentation.
+      <div>
+        <img className=".img" src="assets/images/image-avatar.webp" alt="pic" />
+        <h4>Greg Hooper</h4>
+      </div>
+      </div>
+  );
+}
+
+export default App;
