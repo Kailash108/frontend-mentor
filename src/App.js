0081c12440d98623a105f591e5fe0f8d983feda7
Kailash_mms <srikarkailash9@gmail.com>
2023-12-23 19:21:24 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -4,18 +4,20 @@ function App() {
   return (
     <div className="App">
       <div>
-        <img src="assets/images/illustration-article.svg" alt="qr" />
+        <img className="img1" src="assets/images/illustration-article.svg" alt="qr" />
       </div>
       
       <h3 className="badge">Learning</h3>
-      
-      <p>Published 21 Dec 2023</p>
+
+      <p className="date">Published 21 Dec 2023</p>
 
       <h2 className="heading">HTML & CSS foundations</h2>
-        These languages are the backbone of every website, defining structure, content, and presentation.
+      
+      <p className="desc">These languages are the backbone of every website, defining structure, content, and presentation.</p>
+      <p></p>  
       <div>
-        <img className=".img" src="assets/images/image-avatar.webp" alt="pic" />
-        <h4>Greg Hooper</h4>
+        <img className="img2" src="assets/images/image-avatar.webp" alt="pic" />
+        <h4 className="name">Greg Hooper</h4>
       </div>
       </div>
   );
